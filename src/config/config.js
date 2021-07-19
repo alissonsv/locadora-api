0543a1024e838925db929dc5bb547564d1902e5f
Alisson Vargas <alisson.vargass@hotmail.com>
2021-07-19 13:16:30 -0300

    Refactor: changes variables in db config and add .env.example

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME_DEV,
-    password: process.env.DB_PASSWORD_DEV,
-    database: process.env.DB_DATABASE_DEV,
-    host: process.env.DB_HOST_DEV,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
   },
   production: {
